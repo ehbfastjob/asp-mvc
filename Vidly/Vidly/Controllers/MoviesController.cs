a897a6cbbfe34756cb12e6d6385f6a714ee6221a
ehbfastjob <ehbfastjob@gmail.com>
2016-05-30 17:59:26 +0200

    Initial commit

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ViewResult ShowMovies()
+        {
+            var movies = GetMovies();
+           
+            return View(movies);
+        }
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            return  new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Much Loved"},
+                new Movie {Id = 2  ,Name = "Skyline"},
+                new Movie {Id = 3, Name="Deadpole"}
+            };
+        }
+
+    }
+}
